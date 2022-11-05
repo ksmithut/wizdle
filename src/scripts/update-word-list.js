6f0ca29d31df979e29f2a281e0f46db483f2c045
ksmithut <github@ksmithut.com>
2022-11-04 18:50:16 -0600

    adjusts words

---
+++ src/scripts/update-word-list.js
@@ -0,0 +1,26 @@
+import fs from 'fs/promises'
+
+const MIN_WORD_LENGTH = 3
+
+const source =
+  'https://raw.githubusercontent.com/lorenbrichter/Words/master/Words/en.txt'
+const badWordsSource =
+  'https://raw.githubusercontent.com/coffee-and-fun/google-profanity-words/main/data/list.txt'
+
+const data = await (await fetch(source)).text()
+const badWordsData = await (await fetch(badWordsSource)).text()
+
+const badWordsSet = new Set(badWordsData.trim().split(/\r?\n/))
+
+const words = data
+  .trim()
+  .split(/\r?\n/)
+  .filter(word => {
+    if (word.length < MIN_WORD_LENGTH) return false
+    if (badWordsSet.has(word)) return false
+    if (!word.match(/^[a-z]+$/)) return false
+    return true
+  })
+  .join('\n')
+
+await fs.writeFile(new URL('../game/words.txt', import.meta.url), words)
